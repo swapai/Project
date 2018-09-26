a20d817c38b640d392bed6fe42e8ef9bb302ce2c
Swati Pai <swapai@atlassian.com>
2018-09-25 22:16:43 -0400

    checking taskId

--- Public/api/work.php
+++ Public/api/work.php
@@ -2,7 +2,7 @@
 //get taskId from the URL params
 require '../../app/common.php'
 $taskId = intval($_GET['taskId'] ?? 0);
-
+ echo 'tASKiD'.$taskId;
 //Fetch the work from the db
 $work = Work::findByTaskId($taskId);
 
