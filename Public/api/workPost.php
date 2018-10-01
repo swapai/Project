6cb81a9f00b9ac29e33db3d39b6275cbb24f4ad9
Swati Pai <swapai@atlassian.com>
2018-10-01 10:20:05 -0400

    changes

--- Public/api/workPost.php
+++ Public/api/workPost.php
@@ -1,3 +1,9 @@
 <?php
 
 var_dump($_POST);
+
+$work = new Work($_POST);
+
+$work->create();
+
+echo json_encode($work);
--- app/common.php
+++ app/common.php
@@ -4,6 +4,11 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 
+if($_SERVER['REQUEST_METHOD'] == 'POST' &&
+stripos($_SERVER['CONTENT_TYPE'],'application/json') !== false){
+  $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
 require 'environment.php';
 /** MODELS **/
 require 'models/Work.php';
